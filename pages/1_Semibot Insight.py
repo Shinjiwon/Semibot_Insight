fb287346119b5553a6d000e2df2f5ec284f5b674
NEWG1 <32361608+Shinjiwon@users.noreply.github.com>
2023-12-19 06:44:06 +0000

    20231219 소스 수정

--- pages/1_Semibot Insight.py
+++ pages/1_Semibot Insight.py
@@ -154,10 +154,11 @@ if with_clear_container(submit_clicked):
     answer_container.subheader(f':rainbow[{answer}]')
     # answer_container.subheader(answer)
 
-    for doc in found_docs:
+    for idx, doc in enumerate(found_docs):
+        toggle_key = f"Toggle_Metadata_{idx}"
         answer_container.divider()
 
-        on = st.toggle('Activate feature')
+        on = st.toggle('Activate feature', key=toggle_key)
 
         if on:
             answer_container.write(f"Page Content:{doc.page_content}")
