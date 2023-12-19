2db56106726c59d506470dad838df24c53eb0940
NEWG1 <32361608+Shinjiwon@users.noreply.github.com>
2023-12-19 07:34:08 +0000

    수정

--- pages/1_Semibot Insight.py
+++ pages/1_Semibot Insight.py
@@ -152,7 +152,7 @@ if with_clear_container(submit_clicked):
     )
 
     # answer_container.subheader(f':rainbow[{answer}]')
-    answer_container.expander(f':rainbow[{answer}]')
+    st.expander(f':rainbow[{answer}]')
     # answer_container.subheader(answer)
 
     for idx, doc in enumerate(found_docs):
